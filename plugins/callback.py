7a14927b05aa3ac18e00778f72b79ec7f5aacb0e
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-14 11:37:48 +0530

    Create callback.py

---
+++ plugins/callback.py
@@ -0,0 +1,24 @@
+import pyrogram
+from pyrogram import Client, Filters, InlineKeyboardButton, InlineKeyboardMarkup 
+from translation import Translation
+
+@Client.on_callback_query(Filters.create(lambda _, query: query.data.startswith('about')))          
+async def about_cb(c, m):
+      button = [[InlineKeyboardButton("⛔ Close", callback_data="close"), InlineKeyboardButton("🤔 Help", callback_data="help")]]
+      reply_markup = InlineKeyboardMarkup(button)
+      await m.message.edit(text=Translation.ABOUT,
+                         reply_markup=reply_markup,
+                         disable_web_page_preview=True)
+
+@Client.on_callback_query(Filters.create(lambda _, query: query.data.startswith('help')))          
+async def help_cb(c, m):
+      button = [[InlineKeyboardButton("⛔ Close", callback_data="close"), InlineKeyboardButton("🤖 About", callback_data="about")]]
+      reply_markup = InlineKeyboardMarkup(button)
+      await m.message.edit(text=Translation.HELP,
+                         reply_markup=reply_markup,
+                         disable_web_page_preview=True)
+
+@Client.on_callback_query(Filters.create(lambda _, query: query.data.startswith('close')))          
+async def close_cb(c, m):
+      await m.message.delete()
+      await m.message.reply_to_message.delete()
