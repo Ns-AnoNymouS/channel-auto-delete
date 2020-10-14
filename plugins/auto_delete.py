d8058634b82152fbc3a3ba718a5cc5037b5b5250
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-14 11:02:03 +0530

    Update auto_delete.py

--- plugins/auto_delete.py
+++ plugins/auto_delete.py
@@ -3,6 +3,7 @@ from pyrogram import Client, Filters
 
 @Client.on_message()
 async def delete(c, m):
+  if update.chat.type != "channel":
     if m.from_user is None:
        await m.delete()
     m.continue_propagation()
