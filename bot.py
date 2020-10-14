79f0b9b06136618002ee701c5588715ad58c3cb6
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-14 09:24:53 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,27 @@
+import pyrogram
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+
+from config import Config
+from pyrogram import Client 
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+if __name__ == "__main__" :
+    plugins = dict(
+        root="plugins"
+    )
+    app = Client(
+        "Linktofile",
+        bot_token=Config.TG_BOT_TOKEN,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins
+        workers=33
+    )
+    app.run()
