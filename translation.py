126a3abc57e7e320a2aca7288b83ee928369cff2
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-14 09:26:50 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,35 @@
+class Translation(object):
+      START = """Hi **{}**,
+
+I am a telegram file to permanent link generator bot.
+
+**Maintained By:** [Anonymous](https://t.me/Ns_AnoNymouS)
+"""
+      ABOUT = """**MY DETAILS:**
+
+```🤖My Name:``` [Converter Ns bot](https://t.me/convert_Ns_bot)
+    
+```📝 Language:``` [Python 3](https://www.python.org/)
+
+```🧰 Framework:``` [Pyrogram](https://github.com/pyrogram/pyrogram)
+
+```👨🏻‍💻 Developer:``` [𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬](https://t.me/Ns_AnoNymouS)
+               
+```👨🏻‍🏫 Co-Developer:``` [⚡ᴄʏʙᴇʀ ᴡᴀʀʀɪᴏʀꜱ⚡](https://t.me/CyberW4rriors)
+
+```📢 Channel:``` [NS BOT UPDATES](https://t.me/Ns_bot_updates)
+
+```👥 Group:``` [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
+
+"""
+      HELP = """**__You Don't know How to use me 🤔 ?
+ 
+No problem follow the steps__**
+
+
+1. Send the Media for which you need the permanent link.
+
+2. That's it. Bot will send you the Download link.
+
+If you have any Suggestions tell us in @Ns_Bot_supporters.
+"""
