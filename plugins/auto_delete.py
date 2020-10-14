2e0204a2cc1374ee03efa0821cf6d0737af6acc6
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-14 09:47:07 +0530

    Update auto_delete.py

--- plugins/auto_delete.py
+++ plugins/auto_delete.py
@@ -1,7 +1,7 @@
 import pyrogram 
 from pyrogram import Client, Filters 
 
-@Client.on_message(Filters.text)
+@Client.on_message()
 async def delete(c, m):
     if m.from_user is None:
        await m.delete()
