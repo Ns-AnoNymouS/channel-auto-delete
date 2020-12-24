b345d53d8c88da8726573e0a9a855620b0abb10f
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-12-24 14:53:51 +0530

    Update auto_delete.py

--- plugins/auto_delete.py
+++ plugins/auto_delete.py
@@ -8,5 +8,5 @@ from pyrogram import Client, Filters
 @Client.on_message(Filters.linked_channel)
 async def delete(c, m):
     bot = await c.get_me()
-    print(bot)
+    await m.chat.get_member(get_member)
     await m.delete()
