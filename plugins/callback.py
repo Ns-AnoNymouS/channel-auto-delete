1a6aa54e3d9922c1637ace6ce4779a5bf2aaa5ab
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-04-24 09:49:50 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters as Filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
 from translation import Translation
 
-@Client.on_callback_query(Filters.create(lambda _, query: query.data.startswith('about')))          
+@Client.on_callback_query(Filters.regex('^about$'))       
 async def about_cb(c, m):
       button = [[InlineKeyboardButton("⛔ Close", callback_data="close"), InlineKeyboardButton("🤔 Help", callback_data="help")]]
       reply_markup = InlineKeyboardMarkup(button)
@@ -11,7 +11,7 @@ async def about_cb(c, m):
                          reply_markup=reply_markup,
                          disable_web_page_preview=True)
 
-@Client.on_callback_query(Filters.create(lambda _, query: query.data.startswith('help')))          
+@Client.on_callback_query(Filters.regex('^help$'))         
 async def help_cb(c, m):
       button = [[InlineKeyboardButton("⛔ Close", callback_data="close"), InlineKeyboardButton("🤖 About", callback_data="about")], [InlineKeyboardButton("Add Me To Group 🔰", url="https://t.me/Postdeleter_NsBot?startgroup=False")]]
       reply_markup = InlineKeyboardMarkup(button)
@@ -19,7 +19,7 @@ async def help_cb(c, m):
                          reply_markup=reply_markup,
                          disable_web_page_preview=True)
 
-@Client.on_callback_query(Filters.create(lambda _, query: query.data.startswith('close')))          
+@Client.on_callback_query(Filters.regex('^close$'))          
 async def close_cb(c, m):
       await m.message.delete()
       await m.message.reply_to_message.delete()
